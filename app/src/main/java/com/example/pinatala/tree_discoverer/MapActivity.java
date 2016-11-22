c79a53760d5f630fd05cf655dbdaccf5343084ce
YouYang <youyangr@gmail.com>
2016-11-22 16:46:16 +0100

    Camera function

--- app/src/main/java/com/example/pinatala/tree_discoverer/MapActivity.java
+++ app/src/main/java/com/example/pinatala/tree_discoverer/MapActivity.java
@@ -1,14 +1,40 @@
 package com.example.pinatala.tree_discoverer;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MapActivity extends AppCompatActivity {
 
+    public static final int REQUEST_TAKE_PHOTO = 0;
+    private Uri mMediaUri;
+    private Button cameraButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_map);
+
+        cameraButton = (Button) findViewById(R.id.cameraButton);
+
+        cameraButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                takePhotoIntent.putExtra(MediaStore.EXTRA_OUTPUT, mMediaUri);
+                startActivityForResult(takePhotoIntent, REQUEST_TAKE_PHOTO);
+            }
+        });
     }
+
+
+
+
+
+
+
 }
